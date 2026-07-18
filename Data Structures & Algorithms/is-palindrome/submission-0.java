2913efa27903da738a4a36dcdb990e036cecc956
kondurprabhakarnuthanraju <133472464+kondurprabhakarnuthanraju@users.noreply.github.com>
2026-07-18 19:02:32 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String res="";
+        for(int i=0;i<s.length();i++){
+            if(Character.isLetterOrDigit(s.charAt(i))){
+                res=res+s.charAt(i);
+            }
+        }
+        for(int i=0;i<res.length()/2;i++){
+            if(res.charAt(i)!=res.charAt(res.length()-i-1)){
+                return false;
+
+            }
+        }
+        return true;
+
+
+  }
+}
